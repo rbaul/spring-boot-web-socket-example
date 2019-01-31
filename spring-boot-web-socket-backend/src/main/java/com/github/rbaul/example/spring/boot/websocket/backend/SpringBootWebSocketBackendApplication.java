2b5ae73173ae82d30c3fe905dc55c90742bce044
Roman Baul <Roman.Baul@ecitele.com>
2019-01-31 19:53:54 +0200

    Initial Commit

---
+++ spring-boot-web-socket-backend/src/main/java/com/github/rbaul/example/spring/boot/websocket/backend/SpringBootWebSocketBackendApplication.java
@@ -0,0 +1,14 @@
+package com.github.rbaul.example.spring.boot.websocket.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebSocketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebSocketBackendApplication.class, args);
+	}
+
+}
+
---
+++ spring-boot-web-socket-backend/src/test/java/com/github/rbaul/example/spring/boot/websocket/backend/SpringBootWebSocketBackendApplicationTests.java
@@ -0,0 +1,17 @@
+package com.github.rbaul.example.spring.boot.websocket.backend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootWebSocketBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
